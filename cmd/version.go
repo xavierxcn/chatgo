87dccdeba3f544f7242987d68b9aaa860fa66e4c
xavier <xavier@xavierx.cn>
2023-03-07 20:36:58 +0800

    feat: stream tell

--- cmd/version.go
+++ cmd/version.go
@@ -13,7 +13,7 @@ var versionCmd = &cobra.Command{
 	Short: "show version",
 	Long:  `show version`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("chatgo version: %s", chatgo.Version)
+		fmt.Printf("chatgo version: %s\n", chatgo.Version)
 	},
 }
 
