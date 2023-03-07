13b2986d7e83af4e3c0f2279d9639281dca143d3
xavier <xavier@xavierx.cn>
2023-03-07 20:36:14 +0800

    feat: stream tell

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/xavierxcn/chatgo/chatgo"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "show version",
+	Long:  `show version`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("chatgo version: %s", chatgo.Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
